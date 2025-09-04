0a2a5b077e2b9408d9d17e9b6cd2c7eac711dbd8
inniszao <metelski.ryan@gmail.com>
2025-09-04 13:58:15 -0500

    include glm in shader class

--- shader.h
+++ shader.h
@@ -2,6 +2,7 @@
 #define SHADER_H
 
 #include <glad/glad.h>
+#include <glm/glm.hpp>
 
 #include <string>
 #include <fstream>
@@ -148,7 +149,7 @@ public:
 	{
 		glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
 	}
-
+	
 };
 
 #endif
